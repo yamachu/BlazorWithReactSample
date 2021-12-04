f176f1a3a3782f72aa2ad742dab325817cadb74f
yamachu <yamachu.dev@gmail.com>
2021-12-04 22:49:15 +0900

    Add sample Counter

--- BlazorWebAssembly/Program.cs
+++ BlazorWebAssembly/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWithReactSample;
+using BlazorWithReactSample.Pages;
+using JSComponentGeneration.React;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.RootComponents.RegisterForReact<Counter>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
