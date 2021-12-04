ff3b8db72ad92c0ff1562c62304030d6861484a1
yamachu <yamachu.dev@gmail.com>
2021-12-05 05:07:52 +0900

    Blazor側のクラスメソッドを呼び出せるようにする

--- React/src/blazor/Counter.ts
+++ React/src/blazor/Counter.ts
@@ -1,35 +1,59 @@
 import React from "react";
 import { v4 } from "uuid";
+import { type DotnetReference } from "../../types/dotnetReference";
 import { useBlazor } from "./blazor-react";
 
-export function Counter({ title }: { title: string }) {
-  const componentKey = React.useMemo(() => v4(), []);
-
-  React.useEffect(() => {
-    if (componentKey === undefined) {
-      return;
-    }
-    // @ts-ignore
-    const addFuncCb: EventListener = ({ detail }: CustomEvent) => {
-      console.debug("setDotnetReference");
-      console.dir(detail);
-    };
-    // @ts-ignore
-    const removeFuncCb: EventListener = ({}: CustomEvent) => {
-      console.debug("removeDotnetReference");
-    };
-    window.addEventListener("setDotnetReference", addFuncCb);
-    window.addEventListener("removeDotnetReference", removeFuncCb);
-    return () => {
-      window.removeEventListener("setDotnetReference", addFuncCb);
-      window.removeEventListener("removeDotnetReference", removeFuncCb);
-    };
-  }, [componentKey]);
-
-  const fragment = useBlazor("counter", {
-    title,
-    objectIdentifier: componentKey,
-  });
-
-  return fragment;
+export interface CounterHandler {
+  FooBar: () => void;
 }
+
+export const Counter = React.forwardRef<CounterHandler, { title: string }>(
+  ({ title }, ref) => {
+    const componentKey = React.useMemo(() => v4(), []);
+    const [dotnetRef, setDotnetRef] = React.useState<DotnetReference | null>(null);
+
+    React.useEffect(() => {
+      if (componentKey === undefined) {
+        return;
+      }
+      // @ts-ignore
+      const addFuncCb: EventListener = ({ detail }: CustomEvent) => {
+        setDotnetRef(detail.reference);
+      };
+      // @ts-ignore
+      const removeFuncCb: EventListener = ({}: CustomEvent) => {
+        setDotnetRef(null);
+      };
+      window.addEventListener("setDotnetReference", addFuncCb);
+      window.addEventListener("removeDotnetReference", removeFuncCb);
+      return () => {
+        window.removeEventListener("setDotnetReference", addFuncCb);
+        window.removeEventListener("removeDotnetReference", removeFuncCb);
+      };
+    }, [componentKey]);
+
+    React.useImperativeHandle(
+      ref,
+      () => {
+        return {
+          FooBar: async () => {
+            if (dotnetRef === null) {
+              return
+            }
+            console.debug('FooBar: From React')
+            const fooBarResult = await dotnetRef.invokeMethodAsync("FooBarManaged");
+            console.debug(`FooBar: From React, result: ${fooBarResult}`);
+          },
+        };
+      },
+      [dotnetRef]
+    );
+
+    const fragment = useBlazor("counter", {
+      title,
+      objectIdentifier: componentKey,
+    });
+
+    return fragment;
+  }
+);
